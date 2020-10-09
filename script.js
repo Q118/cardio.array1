eac9073cca3af51af23329c7bdb148c06510d415
Q118 <shelbyfish91@gmail.com>
2020-10-09 12:56:20 -0700

    functionality to add up all the years lived

--- script.js
+++ script.js
@@ -39,23 +39,32 @@
       console.log(inventorNames);
   
   
-  
-  
       // Array.prototype.sort()
       // 3. Sort the inventors by birthdate, oldest to youngest
       const sortedInventors = inventors.sort((a, b) => (a.year > b.year ? 1 : -1));
       console.log(sortedInventors);
   
   
-  
       // Array.prototype.reduce()
       // 4. How many years did all the inventors live all together?
-      // const reducer = ()
+      //reduce allows you to build something on each one
+      /* 
+      before es6... var totalYears = 0;
+                    for (var i = 0; i < inventors.length; i++) {
+                        totalyears += inventors[i].year
+                    }
+
+      */
+      const totalYears = inventors.reduce((total, inventor) => {
+          return total + (inventor.passed - inventor.year);
+      }, 0);
+      console.log("total Years");
+      console.log(totalYears);
   
   
       // 5. Sort the inventors by years lived
       const sortedByYears = inventors.sort((a, b) => (((a.passed - a.year) > (b.passed - b.year)) ? -1 : 1));
-      console.table(sortedInventors);
+      console.table(sortedByYears);
   
   
       // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
@@ -65,7 +74,6 @@
       // 7. sort Exercise
       // Sort the people alphabetically by last name
       const sortedByName = inventors.sort((a, b) => (a.last > b.last ? 1 : -1));
-      
       console.log("name");
       console.table(sortedByName);
   
